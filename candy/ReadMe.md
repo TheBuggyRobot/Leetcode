2b1f988a2c815ded80f67f960e773c4f63a0dcf2
Shanto Shanto <sy670205@dal.ca>
2024-11-17 14:45:40 -0400

    Fixing readme

--- candy/ReadMe.md
+++ candy/ReadMe.md
@@ -1,2 +1,11 @@
 # Problem Description
 ![Problem Description](assets/problem_description.bmp)
+
+# Approach
+## Step 1:
+    `n = ratings.size();` - as per problem description number of children = the size of the ratings array.
+
+    `if(n <= 1) return n;` - if there only 1 child or none i.e. n = 0 or 1, we simply return n;
+## Step 2:
+    Each child must have at least one candy. Therefore, it we can assume that the starting value of result (the return value for the number of total number of candies) can be n = ratings.size().
+## Step 3:
--- candy/candy.cpp
+++ candy/candy.cpp
@@ -3,22 +3,21 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         if(n <= 1) return n;
-        int up = 0, down = 0, result = 1, peak = 0;
+        int up = 0, down = 0, result = n, peak = 0;
         for(int i = 0; i < n - 1; i++) {
             if (ratings[i] < ratings[i + 1]) {
                 up++;
                 down = 0;
                 peak = up;
-                result += 1 + up;
+                result += up;
             } else if (ratings[i] == ratings[i + 1]) {
                 up = 0;
                 down = 0;
                 peak = 0;
-                result += 1;
             } else {
                 down++;
                 up = 0;
-                result += peak >= down ? down : down + 1;
+                result += peak >= down ? down - 1 : down;
             }
         }
         return result;
